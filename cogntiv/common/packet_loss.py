7cb276e7698a1cafee84b2f54fec7d820335f48f
roman_goldstain <roman.goldstain@glassboxdigital.com>
2022-09-28 23:44:52 +0300

    reusing now

--- cogntiv/common/packet_loss.py
+++ cogntiv/common/packet_loss.py
@@ -40,13 +40,13 @@ class OneWithinInterval(LossPolicy):
         delta = now - self.anchor_time
 
         if delta > self.upper:
-            self.anchor_time = time.perf_counter()
+            self.anchor_time = now
             return False
 
         if delta >= self.lower:
             drop = np.random.uniform() > 0.5  # uniform() returns random([0,1)) -> false = [0, 0.5) ; true = [0.5, 1)
             if drop:
-                self.anchor_time = time.perf_counter()
+                self.anchor_time = now
             return drop
 
         return False
